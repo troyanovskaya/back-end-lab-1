c8d372a25ac8b5dee62323a0f393555128a68e08
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-14 21:56:13 +0000

    fix: add request names to postman collection

--- src/schema/Category.js
+++ src/schema/Category.js
@@ -1,3 +1,5 @@
+// const mongoose=require('mongoose');
+// mongoose.connect('mongodb+srv://Ann:fndt75DSk@cluster0.4skxzza.mongodb.net/ToDoApp?retryWrites=true&w=majority');
 class Category{
     constructor(categoryName){
         this.categoryName=categoryName;
